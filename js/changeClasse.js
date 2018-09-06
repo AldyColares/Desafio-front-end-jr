ebdf93c352ded9e37265c127c74c2e3ed61f6b3a
Aldy Colares <aldycolares@gmail.com>
2018-09-06 10:32:58 -0300

    The challenge Finished.

--- js/changeClasse.js
+++ js/changeClasse.js
@@ -1,9 +1,12 @@
 
 window.onload = function () {
     if (screen.width < 640 || screen.height < 480) {
-        let element = document.getElementById('justchangeClasse');
+        let element = document.getElementById('just-change-classe');
         element.classList.remove('container');
         element.classList.add('slider');
+        var paragraph = document.getElementById("add-text");
+        var text = document.createTextNode('Por favor arraste para a direita.');
+        paragraph.appendChild(text);
     }
 }
 
